68c2bad52403386a4f78689f8f9d80203c48e520
Puppala, Nishanth <nipuppala@ea.com>
2023-09-28 22:45:55 +0530

    episode-3

--- App.js
+++ App.js
@@ -1,25 +1,18 @@
-/**
- *          <div id="parent">
- *              <div id="child">
- *                  <h1> I'm h1 tag </h1>
- *                  <h2> I'm h2 tag </h2>
- *              </div>
- *          </div>
- */
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm h1 tag"),
-    React.createElement("h2", {}, "I'm h2 tag"),
-  ])
+const Title = () => (
+  <h1 id="heading" className="h1">
+    Hello World from JSX
+  </h1>
 );
 
-console.log(parent);
+const HeadingComponent = () => (
+  <div id="container">
+    <Title />
+    <h1>Hello World from Fucntional Component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
